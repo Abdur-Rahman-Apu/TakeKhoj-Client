55f207984117655a5015fffc97fcef5ed219ac8a
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-11-29 01:33:49 +0600

    Log in functionality completed

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -11,14 +11,72 @@ import {
   Text,
   VStack,
   useBoolean,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useBoolean();
+  const [loading, setLoading] = useBoolean();
+
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const submitHandler = async () => {
+    setLoading.on();
+    if (!email || !password) {
+      toast({
+        title: "Please fill all empty fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading.off();
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "http://localhost:5000/api/user/login",
+        { email, password },
+        config
+      );
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      toast({
+        title: "Logged In successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading.off();
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Failed to login",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      setLoading.off();
+      console.log(error);
+    }
+  };
 
   return (
     <VStack spacing={6}>
@@ -40,6 +98,8 @@ const Login = () => {
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem" onClick={setShow.toggle}>
             {/* <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
@@ -51,7 +111,9 @@ const Login = () => {
       </FormControl>
 
       {/* Login button  */}
-      <Button w={"full"}>Login</Button>
+      <Button w={"full"} isLoading={loading} onClick={submitHandler}>
+        Login
+      </Button>
       <Button
         w={"full"}
         colorScheme="red"
